ca6c9af6ff0746367e616a7737e78935dc0f424c
Chibueze Ejiofor <chibuezeejiofor@fb.com>
2019-07-05 17:34:59 -0700

    Added 4 files to the git

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeTweeFragment.java
@@ -0,0 +1,106 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ComposeTweeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ComposeTweeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ComposeTweeFragment extends DialogFragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    TwitterClient client;
+    EditText etTweetEdit;
+    TextView tvNumCharacters;
+
+    private String title;
+    private OnFragmentInteractionListener mListener;
+
+    public ComposeTweeFragment() {
+        // Required empty public constructor
+    }
+
+    public static ComposeTweeFragment newInstance() {
+        ComposeTweeFragment fragment = new ComposeTweeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose_twee, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Inflate the layout for this fragment
+        client = TwitterApp.getRestClient(view.getContext());
+        etTweetEdit = view.findViewById(R.id.editTweet);
+        tvNumCharacters = view.findViewById(R.id.tvNumCharacters);
+        getDialog().setTitle("Compose a Tweet");
+        etTweetEdit.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction();
+    }
+}
